b148c58b4fd70630fab65997daa24f75b2c0908e
sauja <pkpadwekar21@gmail.com>
2023-10-01 19:21:52 +0530

    Practice1

---
+++ 02-variable/main.tf
@@ -0,0 +1,18 @@
+variable "fruit" {
+  default = "apple"
+}
+
+variable "fruits" {
+  default = ["apple", "banana"]
+}
+
+variable "fruits_print" {
+  default = {
+    apple=10
+    banana= 20
+  }
+}
+
+output "fruiit" {
+  value = var.fruit
+}
