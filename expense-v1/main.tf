54e848a74c4b677dd23001e917a55fc1a35c45b7
sauja <pkpadwekar21@gmail.com>
2023-10-01 22:14:39 +0530

    Practice1

--- expense-v1/main.tf
+++ expense-v1/main.tf
@@ -1,11 +1,14 @@
 resource "aws_instance" "frontend" {
-  ami           = data.aws_ami.ami.image_id
-  instance_type = "t3.micro"
-  vpc_security_group_ids= [data.aws_security_group.sg.id]
-  tags = {
+  ami                    = data.aws_ami.ami.image_id
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [data.aws_security_group.sg.id]
+  tags                   = {
     Name = "frontend"
   }
+}
 
+resource "null_resource" "frontend" {
+  depends_on = [aws_route53_record.frontend]
   provisioner "local-exec" {
     command =<<EOF
     cd /home/centos/infra-ansible
@@ -25,13 +28,16 @@ resource "aws_route53_record" "frontend" {
 }
 
 resource "aws_instance" "backend" {
-  ami           = data.aws_ami.ami.image_id
-  instance_type = "t3.micro"
-  vpc_security_group_ids= [data.aws_security_group.sg.id]
-  tags = {
+  ami                    = data.aws_ami.ami.image_id
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [data.aws_security_group.sg.id]
+  tags                   = {
     Name = "backend"
   }
-  provisioner "local-exec" {
+}
+  resource "null_resource" "backend" {
+    depends_on = [aws_route53_record.backend]
+    provisioner "local-exec" {
     command =<<EOF
     cd /home/centos/infra-ansible
     git pull
@@ -50,13 +56,16 @@ resource "aws_route53_record" "backend" {
 }
 
 resource "aws_instance" "mysql" {
-  ami           = data.aws_ami.ami.image_id
-  instance_type = "t3.micro"
-  vpc_security_group_ids= [data.aws_security_group.sg.id]
-  tags = {
+  ami                    = data.aws_ami.ami.image_id
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [data.aws_security_group.sg.id]
+  tags                   = {
     Name = "mysql"
   }
-  provisioner "local-exec" {
+}
+  resource "null_resource" "mysql" {
+    depends_on = [aws_route53_record.mysql]
+    provisioner "local-exec" {
     command =<<EOF
     cd /home/centos/infra-ansible
     git pull
