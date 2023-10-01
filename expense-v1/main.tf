a9f8d653f140384e6483a43779c3518056eed905
sauja <pkpadwekar21@gmail.com>
2023-10-01 21:54:30 +0530

    Practice1

--- expense-v1/main.tf
+++ expense-v1/main.tf
@@ -9,7 +9,7 @@ resource "aws_instance" "frontend" {
 
 resource "aws_route53_record" "frontend" {
   zone_id = data.aws_route53_zone.zone.zone_id
-  name    = "frontend.saujpoo.online"
+  name    = "frontend.${var.zone_id}"
   type    = "A"
   ttl     = 300
   records = [aws_instance.frontend.private_ip]
@@ -26,7 +26,7 @@ resource "aws_instance" "backend" {
 
 resource "aws_route53_record" "backend" {
   zone_id = data.aws_route53_zone.zone.zone_id
-  name    = "backend.saujpoo.online"
+  name    = "backend.${var.zone_id}"
   type    = "A"
   ttl     = 300
   records = [aws_instance.backend.private_ip]
@@ -43,7 +43,7 @@ resource "aws_instance" "mysql" {
 
 resource "aws_route53_record" "mysql" {
   zone_id = data.aws_route53_zone.zone.zone_id
-  name    = "mysql.saujpoo.online"
+  name    = "mysql.${var.zone_id}"
   type    = "A"
   ttl     = 300
   records = [aws_instance.mysql.private_ip]
