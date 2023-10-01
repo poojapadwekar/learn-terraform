6a039797379f9fe40da57a5749add327d6c4dac5
sauja <pkpadwekar21@gmail.com>
2023-10-01 19:13:30 +0530

    Practice1

--- 01-resource/main.tf
+++ 01-resource/main.tf
@@ -1,8 +1,16 @@
 resource "aws_instance" "web" {
-  ami           = "03265a0778a880afb"
+  ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
 
   tags = {
     Name = "HelloWorld"
   }
+}
+
+resource "aws_route53_record" "web" {
+  zone_id = Z00236712TJAPP7J1TL57
+  name    = "test.saujpoo.online"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.web.private_ip]
 }
