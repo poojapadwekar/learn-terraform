14115c27a729d2d35e980b5a2c0b8e3c221fa5bb
sauja <pkpadwekar21@gmail.com>
2023-10-01 22:32:50 +0530

    Practice1

--- expense-v1/main.tf
+++ expense-v1/main.tf
@@ -7,6 +7,14 @@ resource "aws_instance" "frontend" {
   }
 }
 
+resource "aws_route53_record" "frontend" {
+  zone_id = data.aws_route53_zone.zone.zone_id
+  name    = "frontend.${var.zone_id}"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.frontend.private_ip]
+}
+
 resource "null_resource" "frontend" {
   depends_on = [aws_route53_record.frontend]
   provisioner "local-exec" {
@@ -18,15 +26,6 @@ resource "null_resource" "frontend" {
     EOF
   }
 }
-
-resource "aws_route53_record" "frontend" {
-  zone_id = data.aws_route53_zone.zone.zone_id
-  name    = "frontend.${var.zone_id}"
-  type    = "A"
-  ttl     = 300
-  records = [aws_instance.frontend.private_ip]
-}
-
 resource "aws_instance" "backend" {
   ami                    = data.aws_ami.ami.image_id
   instance_type          = "t3.micro"
@@ -35,17 +34,7 @@ resource "aws_instance" "backend" {
     Name = "backend"
   }
 }
-  resource "null_resource" "backend" {
-    depends_on = [aws_route53_record.backend]
-    provisioner "local-exec" {
-    command =<<EOF
-    cd /home/centos/infra-ansible
-    git pull
-    sleep 60
-    ansible-playbook -i ${aws_instance.backend.private_ip}, -e ansible_user=centos -e ansible_password=DevOps321 main.yml -e role_name=backend
-    EOF
-  }
-}
+
 
 resource "aws_route53_record" "backend" {
   zone_id = data.aws_route53_zone.zone.zone_id
@@ -53,6 +42,18 @@ resource "aws_route53_record" "backend" {
   type    = "A"
   ttl     = 300
   records = [aws_instance.backend.private_ip]
+}
+
+resource "null_resource" "backend" {
+  depends_on = [aws_route53_record.backend]
+  provisioner "local-exec" {
+    command =<<EOF
+    cd /home/centos/infra-ansible
+    git pull
+    sleep 60
+    ansible-playbook -i ${aws_instance.backend.private_ip}, -e ansible_user=centos -e ansible_password=DevOps321 main.yml -e role_name=backend
+    EOF
+  }
 }
 
 resource "aws_instance" "mysql" {
@@ -63,17 +64,7 @@ resource "aws_instance" "mysql" {
     Name = "mysql"
   }
 }
-  resource "null_resource" "mysql" {
-    depends_on = [aws_route53_record.mysql]
-    provisioner "local-exec" {
-    command =<<EOF
-    cd /home/centos/infra-ansible
-    git pull
-    sleep 60
-    ansible-playbook -i ${aws_instance.mysql.private_ip}, -e ansible_user=centos -e ansible_password=DevOps321 main.yml -e role_name=mysql
-    EOF
-  }
-}
+
 
 resource "aws_route53_record" "mysql" {
   zone_id = data.aws_route53_zone.zone.zone_id
@@ -81,4 +72,15 @@ resource "aws_route53_record" "mysql" {
   type    = "A"
   ttl     = 300
   records = [aws_instance.mysql.private_ip]
+}
+resource "null_resource" "mysql" {
+  depends_on = [aws_route53_record.mysql]
+  provisioner "local-exec" {
+    command =<<EOF
+    cd /home/centos/infra-ansible
+    git pull
+    sleep 60
+    ansible-playbook -i ${aws_instance.mysql.private_ip}, -e ansible_user=centos -e ansible_password=DevOps321 main.yml -e role_name=mysql
+    EOF
+  }
 }
